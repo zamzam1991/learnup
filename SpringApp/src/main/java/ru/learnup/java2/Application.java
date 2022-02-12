6907bed626b11a5e511d46ff46defa494485425f
Danis <mettaworldpeace1991@yahoo.com>
2022-02-12 23:00:53 +0300

    The first version of the application for the sale of tickets for opera events

---
+++ SpringApp/src/main/java/ru/learnup/java2/Application.java
@@ -0,0 +1,48 @@
+package ru.learnup.java2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.learnup.java2.services.OperaEvent;
+import ru.learnup.java2.services.OperaHouse;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class);
+
+        OperaHouse operaHouse = ctx.getBean(OperaHouse.class);
+        OperaEvent figaro = ctx.getBean(OperaEvent.class,
+                "Женитьба Фигаро",
+                "Опера-буффа Моцарта на итальянском языке, написанная на либретто Лоренцо да Понте по одноимённой пьесе Бомарше",
+                16,
+                600);
+        OperaEvent nibelung = ctx.getBean(OperaEvent.class,
+                "Кольцо нибелунга",
+                "Цикл из четырёх эпических опер, основанных на реконструкциях германской мифологии, исландских сагах и средневековой поэме «Песнь о Нибелунгах»",
+                12,
+                600);
+        OperaEvent butterfly = ctx.getBean(OperaEvent.class,
+                "Мадам Баттерфляй",
+                "Опера Джакомо Пуччини в трёх актах (в первой редакции в двух актах) на либретто Луиджи Иллика и Джузеппе Джакоза, по мотивам одноимённой драмы Дэвида Беласко (1900)",
+                0,
+                600);
+
+        //добавление событий в театр
+        operaHouse.add(figaro);
+        operaHouse.add(nibelung);
+        operaHouse.add(butterfly);
+        operaHouse.add("Борис Годунов", " опера Модеста Мусоргского в четырёх действиях с прологом (в восьми, во второй редакции девяти картинах).", 12, 500);
+
+        //все события
+        System.out.println(operaHouse.getAllEvents());
+
+        //покупка и возврат билетов
+        operaHouse.buyTheTickets(figaro, 10, 11, 12, 13);
+        System.out.println(operaHouse.getEvent(figaro.getName()));
+        operaHouse.refundTheTickets(figaro, 12, 13);
+        System.out.println(operaHouse.getEvent(figaro.getName()));
+
+        operaHouse.remove("Кольцо нибелунга");
+        System.out.println(operaHouse.getAllEvents());
+    }
+}
---
+++ SpringApp/src/main/java/ru/learnup/java2/services/OperaHouse.java
@@ -0,0 +1,110 @@
+package ru.learnup.java2.services;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@NoArgsConstructor
+@Scope(value = "singleton")
+public class OperaHouse {
+    @Getter
+    private List<OperaEvent> operaEventList = new ArrayList<>();
+
+    public void add(OperaEvent operaEvent) {
+        operaEventList.add(operaEvent);
+    }
+
+    public void add(String name, String description, int ageRestriction, int ticketsCount) {
+        operaEventList.add(new OperaEvent(name, description,ageRestriction, ticketsCount));
+    }
+
+    public void remove(String name) {
+        for (OperaEvent  operaEvent : operaEventList) {
+            if (operaEvent.getName().equals(name)) {
+                operaEventList.remove(operaEvent);
+                return;
+            }
+        }
+    }
+
+    public List<OperaEvent> getAllEvents() {
+        return operaEventList;
+    }
+
+    public OperaEvent getEvent(String name) {
+        for (OperaEvent operaEvent : operaEventList) {
+            if (operaEvent.getName().equals(name))
+                return operaEvent;
+        }
+        return null;
+    }
+
+    public Ticket buyTheTicket(OperaEvent operaEvent, int seatNumber) {
+        List<Ticket> freeTickets = operaEvent.getAllFreeTickets();
+        for (Ticket ticket : freeTickets) {
+            if (ticket.getSeatNumber() == seatNumber) {
+                ticket.setStatus("Sold");
+                return ticket;
+            }
+        }
+        return null;
+    }
+
+    public List<Ticket> buyTheTickets(OperaEvent operaEvent, int... seatNumbers) {
+        List<Ticket> freeTickets = operaEvent.getAllFreeTickets();
+        List<Ticket> boughtTickets = new ArrayList<>();
+        for (int x = 0; x < seatNumbers.length; x++) {
+            for (Ticket ticket : operaEvent.getTickets()) {
+                if (ticket.getSeatNumber() == seatNumbers[x]) {
+                    ticket.setStatus("Sold");
+                    boughtTickets.add(ticket);
+                }
+            }
+        }
+        return boughtTickets;
+    }
+
+    public void refundTheTicket(OperaEvent operaEvent, int seatNumber) {
+        for (Ticket ticket : operaEvent.getTickets()) {
+            if (ticket.getSeatNumber() == seatNumber) {
+                ticket.setStatus("Free");
+                return;
+            }
+        }
+    }
+
+    public void refundTheTicket(Ticket refundableTicket) {
+        for (Ticket ticket : refundableTicket.getOperaEvent().getTickets()) {
+            if (ticket.equals(refundableTicket)) {
+                ticket.setStatus("Free");
+                return;
+            }
+        }
+    }
+
+    public void refundTheTickets(OperaEvent operaEvent, int... seatNumbers) {
+        for (int seatNumber : seatNumbers) {
+            for (Ticket ticket : operaEvent.getTickets()) {
+                if (ticket.getSeatNumber() == seatNumber) {
+                    ticket.setStatus("Free");
+                }
+            }
+        }
+    }
+
+    public void refundTheTickets(Ticket... refundableTickets) {
+        for (Ticket refundableTicket : refundableTickets) {
+            for (Ticket ticket : refundableTicket.getOperaEvent().getTickets()) {
+                if (ticket.equals(refundableTicket)) {
+                    ticket.setStatus("Free");
+                }
+            }
+        }
+    }
+
+}
